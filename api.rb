fd6e38bfaeaac30f4a8ac23d869badff3169d895
Polo B <paul@bonaud.fr>
2014-02-04 00:50:40 +0100

    Use new ModelController feature of Yodatra for the UserController. Adds a no_route error from the api. Fixes an api format error

--- api.rb
+++ api.rb
@@ -1,6 +1,7 @@
 require 'yodatra/base'
 require 'yodatra/logger'
 require 'yodatra/api_formatter'
+
 require File.expand_path 'app/auth/auth.rb'
 
 class Api < Yodatra::Base
@@ -10,15 +11,16 @@ class Api < Yodatra::Base
   use Yodatra::Logger
   use Yodatra::ApiFormatter do |status, headers, response|
     if headers['Content-Type'] =~ /application\/json/
-      valid = status >= 200 && status < 400
-      data = valid ? 'INJECT' : ''
-      errors = valid ? '' : 'INJECT'
-      wrapper = {:data => data, :errors => errors}.to_json
+      valid = status >= 200 && status < 300
+      data = valid ? '?' : ''
+      errors = valid ? '' : '?'
+      replace = /.\?./
       body = response.empty? ? '' : response.first
-      replace = body.is_a?(String) ? /INJECT/ : /.INJECT./
-      response = wrapper.gsub(replace, body)
+      response = {:data => data, :errors => errors}.to_json.gsub(replace, body)
+
       headers['Content-Length'] = response.length.to_s
     end
+    [status, headers, response]
   end
 
   use PublicController
@@ -26,5 +28,16 @@ class Api < Yodatra::Base
   use Auth
 
   use UsersController
+
+  NO_ROUTE_PROC = lambda do
+    status 400
+    content_type 'application/json'
+    ['Nothing around here'].to_json
+  end
+
+  get //, &NO_ROUTE_PROC
+  post //, &NO_ROUTE_PROC
+  put //, &NO_ROUTE_PROC
+  delete //, &NO_ROUTE_PROC
 
 end
