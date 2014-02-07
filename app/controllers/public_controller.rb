3527f548a9be0b05d6cdca4e306b86df9143b809
Polo B <paul@bonaud.fr>
2014-02-07 23:54:56 +0100

    feat(login): Adds the login route

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -1,4 +1,8 @@
+require File.expand_path 'app/auth/auth.rb'
+
 class PublicController < Yodatra::Base
+
+  API_LOGIN_FAIL = 'Login fail'
 
   get '/status404' do
     status 404
@@ -6,6 +10,19 @@ class PublicController < Yodatra::Base
 
   get '/' do
     'Hello Home'
+  end
+
+  put '/login' do
+    @one = User.find_by_email(params[:identifier])
+
+    if @one.nil?
+      status 400
+      [API_LOGIN_FAIL].to_json
+    else
+      salt2 = Auth.generate_token(@one.email, @one.pbkdf)
+      {:salt1 => @one.salt, :salt2 => salt2}.to_json
+    end
+
   end
 
 end
