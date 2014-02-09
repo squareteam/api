3f52d213d806cc7c0f11b930d6aa7223735d5e53
Polo B <paul+st@bonaud.fr>
2014-02-09 23:21:11 +0100

    feat(logout): Add a logout route to....  logout

--- app/controllers/private_controller.rb
+++ app/controllers/private_controller.rb
@@ -2,9 +2,17 @@ class PrivateController < Yodatra::Base
 
   before do
     content_type 'application/json'
+    @cache ||= Cache.new
+  end
+
+  get '/logout' do
+    identifier = request.env['REMOTE_USER']
+    @cache.rm_cache "#{identifier}:SALT2"
+    @cache.rm_cache "#{identifier}:TOKEN"
+    'OK'.to_json
   end
 
   get '/private' do
-    ['You are authenticated and seeing a private area']
+    'You are authenticated and seeing a private area'.to_json
   end
 end
