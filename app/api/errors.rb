4d35f59f631c509257da36e92f0d2ca8545de28a
Paul B <paul+st@bonaud.fr>
2014-02-09 23:20:29 +0100

    chore(api): Extract error message in the errors class

--- app/api/errors.rb
+++ app/api/errors.rb
@@ -2,4 +2,5 @@ class Errors
   LOGIN_FAIL = 'Login fail'
   ID_NOT_FOUND = 'Your email does not exist, therefore you do not exist..'
   NO_PASSWORD_PROVIDED = 'No password given'
+  NO_ROUTE = 'Nothing around here'
 end
