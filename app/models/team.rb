f38d027c5d4aae3d98b3553839619a1b22c1bd46
Polo B <paul+st@bonaud.fr>
2014-05-13 14:20:17 +0200

    style(errors): rename all validation errors in the format api.underscore_message

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,7 +1,6 @@
 class Team < ActiveRecord::Base
-  validates_presence_of :name, :organization
-  validates_uniqueness_of :name
-  validates_uniqueness_of :name, :scope => [:organization]
+  validates_presence_of :name, :organization, message: 'api.missing'
+  validates_uniqueness_of :name, scope: [:organization], message: 'api.already_taken'
 
   has_many :roles
   belongs_to :organization
