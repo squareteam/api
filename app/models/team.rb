798d1b90174abb7fa67819964120b86a2fe2fe70
Polo B <paul@bonaud.fr>
2014-05-15 00:47:55 +0200

    style(errors/msgs): and fix tests around errors msgs

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,6 +1,6 @@
 class Team < ActiveRecord::Base
-  validates_presence_of :name, :organization, message: 'api.{{value}}_missing'
-  validates_uniqueness_of :name, scope: [:organization], message: 'api.{{value}}_already_taken'
+  validates_presence_of :name, :organization, message: 'api.missing'
+  validates_uniqueness_of :name, scope: [:organization], message: 'api.%{value}_already_taken'
 
   has_many :roles
   belongs_to :organization
