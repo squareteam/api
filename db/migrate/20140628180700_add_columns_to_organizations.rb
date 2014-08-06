676407e86256094c0a28436e8f945dcd23f80638
Polo B <paul@bonaud.fr>
2014-08-06 14:40:59 +0200

    fix(migrations): Remove old column in Organization

--- db/migrate/20140628180700_add_columns_to_organizations.rb
+++
@@ -1,8 +0,0 @@
-# 20140517120313
-class AddColumnsToOrganizations < ActiveRecord::Migration
-  def change
-    change_table :organizations do |t|
-      t.integer :admin_team_id, :null => true, :default => 0
-    end
-  end
-end
