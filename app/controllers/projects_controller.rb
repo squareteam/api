d4eba8b00f1e0f26123cc1f20fc75ef64d1cc4a4
Charly POLY <cpoly55@gmail.com>
2014-08-13 00:15:46 +0200

    feat(projects_controller): add controller for projects

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,25 @@
+require 'yodatra/models_controller'
+
+class ProjectsController < Yodatra::ModelsController
+
+  def read_scope
+    self.class.read_scope
+  end
+
+  def project_params
+    params.select { |k, _| %w(title description deadline).include?(k.to_s) }
+  end
+
+  class << self
+    def read_scope
+      {
+        only: [:title, :description, :deadline],
+        # TODO include :
+        #   - missions  number
+        #   - members   number
+        #   - documents number
+      }
+    end
+  end
+
+end
