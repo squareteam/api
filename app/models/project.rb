cc33f7655db604cd51eb16c345efbdde3dff38f1
Charly POLY <cpoly55@gmail.com>
2014-08-14 16:39:54 +0200

    feat(models/projects): projects associations done right

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,4 +2,7 @@ class Project < ActiveRecord::Base
 
    has_many :missions
 
+   has_and_belongs_to_many :projects, -> { where( project_accesses: { model_type: 'Organization' } ) }, join_table: 'project_accesses', association_foreign_key: 'model_id'
+   has_and_belongs_to_many :projects, -> { where( project_accesses: { model_type: 'User' } ) }, join_table: 'project_accesses', association_foreign_key: 'model_id'
+
 end
