7cba00ba3c9e89e2e72dba9fcd93f0230697eb5c
Polo B <paul@bonaud.fr>
2014-09-16 22:51:31 +0200

    chore(schema): update schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140915220040) do
+ActiveRecord::Schema.define(version: 20140916192600) do
 
   create_table "knowledges", force: true do |t|
     t.string   "title",      limit: 100
@@ -78,11 +78,12 @@ ActiveRecord::Schema.define(version: 20140915220040) do
   end
 
   create_table "teams", force: true do |t|
-    t.string   "name",            limit: 125,                 null: false
-    t.integer  "organization_id",                             null: false
+    t.string   "name",            limit: 125,                     null: false
+    t.integer  "organization_id",                                 null: false
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.boolean  "is_admin",                    default: false, null: false
+    t.boolean  "is_admin",                    default: false,     null: false
+    t.string   "color",           limit: 7,   default: "#2cc1ff", null: false
   end
 
   add_index "teams", ["name", "organization_id"], name: "index_teams_on_name_and_organization_id", unique: true, using: :btree
