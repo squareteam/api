04208689ba8cb99daa8ac1ba069fbf07b9163217
Polo B <paul@bonaud.fr>
2014-10-18 00:18:23 +0200

    chore(initializer/mailer): read config per env

--- config/application.rb
+++ config/application.rb
@@ -1,9 +1,15 @@
+ENV['RACK_ENV'] ||= 'development'
+
 module Squareteam
   class Application
     CONFIG = OpenStruct.new
 
     def self.configure
       yield(CONFIG) if block_given?
+    end
+
+    def self.environment
+      ENV['RACK_ENV']
     end
   end
 end
