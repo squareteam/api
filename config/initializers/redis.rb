2729c34197000b7c289fd56ff1e447710071c574
Polo B <paul+st@bonaud.fr>
2014-11-30 21:45:57 +0100

    Update yodatra for latest logger and adapt multi env redis conf

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,3 +1,3 @@
 Squareteam::Application.configure do |config|
-  config.redis = YAML.load(ERB.new(File.read(File.dirname(__FILE__) + "/../redis.yml")).result).symbolize_keys
+  config.redis = YAML.load(ERB.new(File.read(File.dirname(__FILE__) + "/../redis.yml")).result).symbolize_keys[Squareteam::Application.environment.to_sym]
 end
